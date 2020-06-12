d4f21a6be74a6c371c6663243e7b93dd9320cab1
captaincurry94 <leo.boes@yahoo.com>
2020-06-12 19:21:10 +0200

    Splitter.split now outputs words in lowercase

--- Splitter.py
+++ Splitter.py
@@ -1,8 +1,13 @@
-def splitter (list):
+def split (list):
     wordsCut = []
     for answer in list:
         sentence = answer.split()
         for word in sentence:
-            wordsCut.append(word)
+            wordsCut.append(word.lower())
     return wordsCut
+
+def count(list=[]):
+    uniques = set(list)
+    counter = {}
+
 
--- main.py
+++ main.py
@@ -5,7 +5,10 @@ def answers_to_array(input_xlsx, sheet_no = 0, answer_column = 'answer_text'):
     Arguments:
         input_xlsx (str): path of input file
         sheet_no (int): index of the sheet to be accessed (zero-indexed)
-        answer_column (str): name of column with answers"""
+        answer_column (str): name of column with answers
+
+    Returns:
+        array of strings with answers"""
 
     answers = list(pd.read_excel(input_xlsx, sheet_name = sheet_no)[answer_column])
     answers_cleaned = [element.replace("\n+","").replace("\n","") for element in answers]
