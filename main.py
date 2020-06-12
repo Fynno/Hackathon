3b0365c2ebc83a6592739c75bf159ca390546098
captaincurry94 <leo.boes@yahoo.com>
2020-06-12 18:31:27 +0200

    can now import excel file

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from Test import splitter
+import pandas as pd
 
-list = ("Hallo","Leo hat einen sehr kleinen Kopf","Stinker stinken","Pustekuchen")
-print(splitter(list))
+def answers_to_array(input_xlsx, sheet_no = 0, answer_column):
+    """Returns array of answers from excel file
 
+    Arguments:
+        input_xlsx (str): path of input file
+        sheet_no (int): index of the sheet to be accessed (zero-indexed)
+        answer_column (str): name of column with answers"""
 
+    return list(pd.read_excel(input_xlsx, sheet_name = sheet_no)[answer_column])
