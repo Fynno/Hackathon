3bea4bc871f15d71aad01575efdc64bf05b0bf24
captaincurry94 <leo.boes@yahoo.com>
2020-06-12 18:46:25 +0200

    can now clean imported array from \n statements

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-def answers_to_array(input_xlsx, sheet_no = 0, answer_column):
+def answers_to_array(input_xlsx, sheet_no = 0, answer_column = 'answer_text'):
     """Returns array of answers from excel file
 
     Arguments:
         input_xlsx (str): path of input file
         sheet_no (int): index of the sheet to be accessed (zero-indexed)
         answer_column (str): name of column with answers"""
+    answers = list(pd.read_excel(input_xlsx, sheet_name = sheet_no)[answer_column])
+    answers_cleaned = [element.replace("\n+","").replace("\n","") for element in answers]
+    return answers_cleaned
 
-    return list(pd.read_excel(input_xlsx, sheet_name = sheet_no)[answer_column])
+data = answers_to_array('Freitext.xls',sheet_no = 0,answer_column = 'answer_text')
