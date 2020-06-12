99fe7e9335bb5c0356a8faee0f2b1ce74a963da8
FinnEhrenberg <66836406+FinnEhrenberg@users.noreply.github.com>
2020-06-12 17:54:27 +0200

    erster commit

---
+++ Finn.py
@@ -0,0 +1,8 @@
+answers = ["penis", "fynn", "leohard", "Affe mit Waffe"]
+print(answers)
+
+
+def answers_upper(element, answers):
+    for element in answers:
+        element.upper()
+    print(element)
