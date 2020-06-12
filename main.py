e7e87ed1d5b1ee39c8cc76afd8d1010c80df3e0d
Fynn Knudsen <f.knudsen@tuhh.de>
2020-06-12 18:52:58 +0200

    Merge

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import pandas as pd
 
-<<<<<<< HEAD
-def answers_to_array(input_xlsx, sheet_no = 0, answer_column):
-=======
 def answers_to_array(input_xlsx, sheet_no = 0, answer_column = 'answer_text'):
     """Returns array of answers from excel file
-
->>>>>>> 3bea4bc871f15d71aad01575efdc64bf05b0bf24
     Arguments:
         input_xlsx (str): path of input file
         sheet_no (int): index of the sheet to be accessed (zero-indexed)
         answer_column (str): name of column with answers"""
+
     answers = list(pd.read_excel(input_xlsx, sheet_name = sheet_no)[answer_column])
     answers_cleaned = [element.replace("\n+","").replace("\n","") for element in answers]
     return answers_cleaned
