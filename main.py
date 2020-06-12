8447697dc00c27248ad91571969cd16bab587e1e
Fynn Knudsen <f.knudsen@tuhh.de>
2020-06-12 18:39:32 +0200

    merged

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
 
 def answers_to_array(input_xlsx, sheet_no = 0, answer_column):
-    """Returns array of answers from excel file
-
     Arguments:
         input_xlsx (str): path of input file
         sheet_no (int): index of the sheet to be accessed (zero-indexed)
